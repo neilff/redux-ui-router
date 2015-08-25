6ff1e2d3a2630c426a2308f66f9fccf4568cbf12
Neil Fenton <neilff@gmail.com>
2015-08-24 20:03:26 -0400

    updated deps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import routerState from './router-state-reducer';
 
 import routerMiddleware from './router-middleware';
 import routerActions from './router-actions';
 import uiRouterListener from './router-listener';
+
+export const router = routerState;
 
 export default angular
   .module('ng-ui-router-middleware', [
