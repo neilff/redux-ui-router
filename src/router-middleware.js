6a26e4170c7f71bd1f516dfec69d4ad17faa9d39
hally9k <dalllorzo8>
2016-07-21 10:53:52 +1200

    Updated dependencies to fix babel-eslint 'estraverse-fb' dep issue

--- src/router-middleware.js
+++ src/router-middleware.js
@@ -2,7 +2,7 @@ import {
   STATE_GO,
   STATE_RELOAD,
   STATE_TRANSITION_TO,
-  STATE_CHANGE_SUCCESS
+  STATE_CHANGE_SUCCESS,
 } from './action-types';
 
 export default function routerMiddleware($state) {
@@ -10,31 +10,31 @@ export default function routerMiddleware($state) {
     const { payload } = action;
 
     switch (action.type) {
-      case STATE_GO:
-        return $state.go(payload.to, payload.params, payload.options)
+    case STATE_GO:
+      return $state.go(payload.to, payload.params, payload.options)
           .then(next(action));
 
-      case STATE_RELOAD:
-        return $state.reload(payload)
+    case STATE_RELOAD:
+      return $state.reload(payload)
           .then(next(action));
 
-      case STATE_TRANSITION_TO:
-        return $state.transitionTo(payload.to, payload.params, payload.options)
-          .then(next(action));
+    case STATE_TRANSITION_TO:
+      return $state.transitionTo(payload.to, payload.params, payload.options)
+      .then(next(action));
 
-      case STATE_CHANGE_SUCCESS:
-        return next({
-          type: STATE_CHANGE_SUCCESS,
-          payload: {
-            currentState: $state.current,
-            currentParams: $state.params,
-            prevState: getState().router.currentState,
-            prevParams: getState().router.currentParams,
-          },
-        });
+    case STATE_CHANGE_SUCCESS:
+      return next({
+        type: STATE_CHANGE_SUCCESS,
+        payload: {
+          currentState: $state.current,
+          currentParams: $state.params,
+          prevState: getState().router.currentState,
+          prevParams: getState().router.currentParams,
+        },
+      });
 
-      default:
-        return next(action);
+    default:
+      return next(action);
     }
   };
 }
