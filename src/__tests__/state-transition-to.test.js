d5d2d9175348eeaf1af581a07a2f9d6696b1f6df
hally9k <hally9k@gmail.com>
2016-07-20 19:32:33 +1200

    Revert "Fixed incorrect arg name in state-transition-to.js from 'toParams' to 'params'..."

--- src/__tests__/state-transition-to.test.js
+++ src/__tests__/state-transition-to.test.js
@@ -5,7 +5,7 @@ describe('stateTransitionTo', () => {
   it('should create an action containing the state to transition to', () => {
     let action = stateTransitionTo('example', { test: 'hello' }, { option: 'world' });
     expect(action.payload.to).to.equal('example');
-    expect(action.payload.params.test).to.equal('hello');
+    expect(action.payload.toParams.test).to.equal('hello');
     expect(action.payload.options.option).to.equal('world');
     expect(action.type).to.equal('@@reduxUiRouter/transitionTo');
   });
@@ -13,7 +13,7 @@ describe('stateTransitionTo', () => {
   it('should create an action when to params are undefined', () => {
     let action = stateTransitionTo('example', undefined, undefined);
     expect(action.payload.to).to.equal('example');
-    expect(action.payload.params).to.equal(undefined);
+    expect(action.payload.toParams).to.equal(undefined);
     expect(action.payload.options).to.equal(undefined);
     expect(action.type).to.equal('@@reduxUiRouter/transitionTo');
   });
