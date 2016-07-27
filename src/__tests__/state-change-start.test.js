fed47f0b24d64fe274172304b3e95609764926c7
Mark Potnick <mark.potnick@gmail.com>
2016-07-27 09:11:57 -0400

    Adding test

--- src/__tests__/state-change-start.test.js
+++ src/__tests__/state-change-start.test.js
@@ -3,12 +3,13 @@ import stateChangeStart from '../state-change-start';
 
 describe('stateChangeStart', () => {
   it('should create an action with the provided params', () => {
-    let action = stateChangeStart('evt', 'toState', 'toParams', 'fromState', 'fromParams');
+    let action = stateChangeStart('evt', 'toState', 'toParams', 'fromState', 'fromParams', 'options');
 
     expect(action.payload.toState).to.equal('toState');
     expect(action.payload.toParams).to.equal('toParams');
     expect(action.payload.fromState).to.equal('fromState');
     expect(action.payload.fromParams).to.equal('fromParams');
+    expect(action.payload.options).to.equal('options');
     expect(action.payload.evt).to.equal('evt');
     expect(action.type).to.equal('@@reduxUiRouter/$stateChangeStart');
   });
