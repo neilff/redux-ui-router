4b5d09a009a1d14cc28f5f7d53c6aa002448228a
Antoine Savignac <Nighthawk14@users.noreply.github.com>
2016-09-08 04:35:18 +1000

    fix #66 missing parameters passed on transition onStart. Add tests for router-listener (#67)

--- src/router-listener.js
+++ src/router-listener.js
@@ -2,12 +2,11 @@
  * Listens for events emitted from Angular UI Router and fires redux events
  *
  * @param {object} $transitions Dependency
- * @param {object} $urlRouter Dependency
  * @param {object} ngUiStateChangeActions Dependency
  * @return {undefined} undefined
  */
-export default function RouterListener($transitions, $urlRouter, ngUiStateChangeActions) {
-  const prevNext = (t) => [t.to(), t.to().params, t.from(), t.from().params];
+export default function RouterListener($transitions, ngUiStateChangeActions) {
+  const prevNext = (t) => [t.to(), t.params('to'), t.from(), t.params('from')];
 
   $transitions.onStart({}, ($transition$) => ngUiStateChangeActions.onStateChangeStart(...prevNext($transition$)));
   $transitions.onError({}, ($transition$) => ngUiStateChangeActions.onStateChangeError(...prevNext($transition$), $transition$.error()));
@@ -16,6 +15,5 @@ export default function RouterListener($transitions, $urlRouter, ngUiStateChange
 
 RouterListener.$inject = [
   '$transitions',
-  '$urlRouter',
   'ngUiStateChangeActions',
 ];
