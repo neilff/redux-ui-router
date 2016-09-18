9bd4b436ce6d76d28a0bb733ef9924e8f2f761df
hally9k <hally9k@gmail.com>
2016-09-19 10:44:25 +1200

    Fixed the prev state and params in the redux router state getting set… (#68)
    * Fixed the prev state and params in the redux router state getting set to the current state and params by changing the way that the STATE_CHANGE_SUCCESS handler in the middleware accesses the previous state and params.

--- src/__tests__/state-change-success.test.js
+++ src/__tests__/state-change-success.test.js
@@ -3,7 +3,11 @@ import stateChangeSuccess from '../state-change-success';
 
 describe('stateChangeSuccess', () => {
   it('should create an action with the provided params', () => {
-    let action = stateChangeSuccess();
+    let action = stateChangeSuccess('toState', 'toParams', 'fromState', 'fromParams');
+    expect(action.payload.toState).to.equal('toState');
+    expect(action.payload.toParams).to.equal('toParams');
+    expect(action.payload.fromState).to.equal('fromState');
+    expect(action.payload.fromParams).to.equal('fromParams');
     expect(action.type).to.equal('@@reduxUiRouter/onSuccess');
   });
 });
--- src/router-listener.js
+++ src/router-listener.js
@@ -6,11 +6,28 @@
  * @return {undefined} undefined
  */
 export default function RouterListener($transitions, ngUiStateChangeActions) {
-  const prevNext = (t) => [t.to(), t.params('to'), t.from(), t.params('from')];
+  const prevNext = t => [t.to(), t.params('to'), t.from(), t.params('from')];
 
-  $transitions.onStart({}, ($transition$) => ngUiStateChangeActions.onStateChangeStart(...prevNext($transition$)));
-  $transitions.onError({}, ($transition$) => ngUiStateChangeActions.onStateChangeError(...prevNext($transition$), $transition$.error()));
-  $transitions.onSuccess({}, () => ngUiStateChangeActions.onStateChangeSuccess());
+  const prevNextReduxState = t => ([getStateObject(t.to()),
+                                            t.params('to'),
+                                            getStateObject(t.from()),
+                                            t.params('from')]);
+
+  $transitions.onStart({}, $transition$ => ngUiStateChangeActions.onStateChangeStart(...prevNext($transition$)));
+  $transitions.onError({}, $transition$ => ngUiStateChangeActions.onStateChangeError(...prevNext($transition$), $transition$.error()));
+  $transitions.onSuccess({}, $transition$ => ngUiStateChangeActions.onStateChangeSuccess(...prevNextReduxState($transition$)));
+}
+
+function getStateObject(state) {
+  if (!state) {
+    return {};
+  }
+  const { name, params, url } = state;
+  return {
+    name,
+    params,
+    url,
+  };
 }
 
 RouterListener.$inject = [
