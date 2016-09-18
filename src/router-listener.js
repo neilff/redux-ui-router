04035a7967ad81141ce153782d77c5e1d4c88fc8
hally9k <hally9k@gmail.com>
2016-09-19 09:58:13 +1200

    fixed state object

--- src/router-listener.js
+++ src/router-listener.js
@@ -6,25 +6,26 @@
  * @return {undefined} undefined
  */
 export default function RouterListener($transitions, ngUiStateChangeActions) {
-  const prevNext = (t) => [t.to(), t.params('to'), t.from(), t.params('from')];
+  const prevNext = t => [t.to(), t.params('to'), t.from(), t.params('from')];
 
-  const prevNextReduxState = (t) => {
-    const toState = t.to();
-    let toStateName = '';
-    if (toState.name) {
-      toStateName = toState.name;
-    }
-    const fromState = t.from();
-    let fromStateName = '';
-    if (fromState.name) {
-      fromStateName = fromState.name;
-    }
-    return [toStateName, t.params('to'), fromStateName, t.params('from')];
-  };
+  const prevNextReduxState = t => ([getStateObject(t.to()),
+                                            t.params('to'),
+                                            getStateObject(t.from()),
+                                            t.params('from')]);
 
-  $transitions.onStart({}, ($transition$) => ngUiStateChangeActions.onStateChangeStart(...prevNext($transition$)));
-  $transitions.onError({}, ($transition$) => ngUiStateChangeActions.onStateChangeError(...prevNext($transition$), $transition$.error()));
-  $transitions.onSuccess({}, ($transition$) => ngUiStateChangeActions.onStateChangeSuccess(...prevNextReduxState($transition$)));
+  $transitions.onStart({}, $transition$ => ngUiStateChangeActions.onStateChangeStart(...prevNext($transition$)));
+  $transitions.onError({}, $transition$ => ngUiStateChangeActions.onStateChangeError(...prevNext($transition$), $transition$.error()));
+  $transitions.onSuccess({}, $transition$ => ngUiStateChangeActions.onStateChangeSuccess(...prevNextReduxState($transition$)));
+}
+
+function getStateObject(state) {
+  if(!state){ return {}; }
+  let { name, params, url } = state;
+  return {
+    name,
+    params,
+    url
+  }
 }
 
 RouterListener.$inject = [
