ecfac55c7f90adbfb93d6ac28f75bf0550cae82a
Stephane Rufer <stephane.rufer@gmail.com>
2016-09-20 19:24:30 -0700

    Add `href` property to the router state object (#51)
    * Add `href` property to the router state object
    
    Adding `href` to the router state object gives access to the
    `$state.href` function in React code. This allows the creation
    of urls for states, which is critical when a component links to
    another part of the app and should be crawlable by a crawler.
    
    * Update licenser test
    
    Include `href` payload key in tests

--- src/__tests__/router-listener.test.js
+++ src/__tests__/router-listener.test.js
@@ -44,7 +44,7 @@ describe('routerListener', () => {
     describe('onStart', () => {
       it('must call onStateChangeStart with the transition parameters', () => {
         $transitions.onStart.yield($transition$);
-        expect(ngUiStateChangeActions.onStateChangeStart.calledWith('to', 'toParams', 'from', 'fromParams')).to.equal(true);
+        expect(ngUiStateChangeActions.onStateChangeStart.calledWith('to', 'toParams', 'from', 'fromParams', 'href')).to.equal(true);
       });
     });
 
@@ -52,7 +52,7 @@ describe('routerListener', () => {
       it('must call onStateChangeError with the transition parameters and the transition Error', () => {
         $transition$.error = sinon.stub().returns('transitionError');
         $transitions.onError.yield($transition$);
-        expect(ngUiStateChangeActions.onStateChangeError.calledWith('to', 'toParams', 'from', 'fromParams', 'transitionError')).to.equal(true);
+        expect(ngUiStateChangeActions.onStateChangeError.calledWith('to', 'toParams', 'from', 'fromParams', 'href', 'transitionError')).to.equal(true);
       });
     });
 
