c4a562dd3f84d6220a6da5af0751c1e793f90997
Mohsen Najaflou <najaflou@inside.sahab.ir>
2017-05-22 13:41:00 +0430

    update `angular-ui-router` name to `@uirouter/angularjs`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+import uiRouter from '@uirouter/angularjs';
 import routerState from './router-state-reducer';
 import stateGo from './state-go';
 import stateReload from './state-reload';
