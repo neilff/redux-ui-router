ddd8cc3bfaaf14f5aa10b2e860a456d23293da1f
hally9k <hally9k@gmail.com>
2017-06-04 14:52:21 +1200

    Upgraded to new ui-router package name

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+import uiRouter from '@uirouter/angularjs';
 import routerState from './router-state-reducer';
 import stateGo from './state-go';
 import stateReload from './state-reload';
@@ -11,19 +11,12 @@ import stateChangeActions from './state-change-actions';
 import stateActivationActions from './state-activation-actions';
 
 export default angular
-  .module('ng-ui-router-middleware', [
-    uiRouter,
-  ])
+  .module('ng-ui-router-middleware', [uiRouter])
   .provider('ngUiStateChangeActions', stateChangeActions)
   .factory('ngUiRouterMiddleware', routerMiddleware)
   .factory('ngUiStateActivationActions', stateActivationActions)
-  .run(uiRouterListener)
-  .name;
+  .run(uiRouterListener).name;
 
 export const router = routerState;
 
-export {
-  stateGo,
-  stateReload,
-  stateTransitionTo,
-};
+export { stateGo, stateReload, stateTransitionTo };
